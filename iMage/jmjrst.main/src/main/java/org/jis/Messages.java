c5707fb77952082004e5dc389481edd5895620ff
Christopher Homberger <christopher.homberger@web.de>
2019-05-01 23:56:36 +0200

    Fixed Messages getString curly Brackets style

--- iMage/jmjrst.main/src/main/java/org/jis/Messages.java
+++ iMage/jmjrst.main/src/main/java/org/jis/Messages.java
@@ -47,14 +47,10 @@ public class Messages implements Serializable {
    *          the message key
    * @return returns the message of the specified language
    */
-  public String getString(String key)
-  {
-    try
-    {
+  public String getString(String key) {
+    try {
       return resourceBundle.getString(key);
-    }
-    catch (MissingResourceException e)
-    {
+    } catch (MissingResourceException e) {
       e.printStackTrace();
       return '!' + key + '!';
     }

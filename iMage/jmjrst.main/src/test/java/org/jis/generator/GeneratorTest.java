41856f2c8dade75507204fe186aee2de7c0bc83f
Christopher Homberger <christopher.homberger@web.de>
2019-05-02 13:17:24 +0200

    Added RotateImageTestImageIsNullNoRotate Test

--- iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
+++ iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
@@ -1,5 +1,7 @@
 package org.jis.generator;
 
+import static org.junit.Assert.assertNull;
+
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -7,6 +9,7 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * GeneratorTest Test the Generator for working correctly
@@ -23,5 +26,13 @@ public class GeneratorTest {
     public static void setUp() throws IOException {
         generator = new Generator(null, 0);
         image = ImageIO.read(new File(GeneratorTest.class.getResource("/image.jpg").getFile()));
+    }
+
+    /**
+     * Tests rotating null Image without rotate
+     */
+    @Test
+    public void rotateImageTestImageIsNullNoRotate() {
+        assertNull(generator.rotateImage(null, 0.0));
     }
 }

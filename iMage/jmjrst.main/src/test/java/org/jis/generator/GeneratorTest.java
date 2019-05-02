c9daceefe93fe1a0d92b2ab947ca11ca3b9dd089
Christopher Homberger <christopher.homberger@web.de>
2019-05-02 13:13:19 +0200

    Added Basic junit GeneratorTest class

---
+++ iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
@@ -0,0 +1,27 @@
+package org.jis.generator;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.junit.BeforeClass;
+
+/**
+ * GeneratorTest Test the Generator for working correctly
+ */
+public class GeneratorTest {
+    private static Generator generator;
+    private static BufferedImage image;
+
+    /**
+     * Prepares Generator and BufferedImage objects for all Tests
+     * @throws IOException image.png not found
+     */
+    @BeforeClass
+    public static void setUp() throws IOException {
+        generator = new Generator(null, 0);
+        image = ImageIO.read(new File(GeneratorTest.class.getResource("/image.jpg").getFile()));
+    }
+}

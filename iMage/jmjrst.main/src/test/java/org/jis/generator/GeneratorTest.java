ea7e32fd1409348f7dd4f282f5fa6d1b3d78bc7c
Christopher Homberger <christopher.homberger@web.de>
2019-05-06 19:02:58 +0200

    Add additional Tests

--- iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
+++ iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
@@ -8,14 +8,20 @@ import static org.junit.Assert.assertSame;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Vector;
+import java.util.zip.ZipInputStream;
 
 import javax.imageio.ImageIO;
 
 import org.junit.After;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -23,6 +29,7 @@ import org.junit.Test;
  */
 public class GeneratorTest {
     private static Generator generator;
+    private static File imagefile;
     private static BufferedImage image;
 
     private BufferedImage nimage = null;
@@ -38,7 +45,8 @@ public class GeneratorTest {
             dir.mkdir();
         }
         generator = new Generator(null, 0);
-        image = ImageIO.read(new File(GeneratorTest.class.getResource("/image.jpg").getFile()));
+        imagefile = new File(GeneratorTest.class.getResource("/image.jpg").getFile());
+        image = ImageIO.read(imagefile);
     }
 
     /**
@@ -145,6 +153,109 @@ public class GeneratorTest {
             for (int i = 0; i < image.getWidth(); i++) {
                 assertEquals("False Image i=" + i + " j=" + j, image.getRGB(i, j),
                 nimage.getRGB(j, nimage.getHeight() - 1 - i));
+            }
+        }
+    }
+
+    /**
+     * Tests racing conditions of generateText
+     * from null
+     */
+    @Test(expected = NullPointerException.class)
+    public void generateTextTestfromIsNull() {
+        generator.generateText(null,
+        new File("target/test"), 0, 0);
+    }
+
+    /**
+     * Tests racing conditions of generateText
+     * to null
+     */
+    @Test(expected = NullPointerException.class)
+    public void generateTextTesttoIsNull() {
+        generator.generateText(
+        new File("target/test"), null, 0, 0);
+    }
+
+    /**
+     * Tests resizing of generateText image
+     * @throws IOException output image couldn't be written / read / no outputfile
+     * Ignored for now missing Outputfile
+     */
+    @Test
+    @Ignore("Need Main Object!")
+    public void generateTextTestResize() throws IOException {
+        var to = new File("target/test/image_resized_" + System.currentTimeMillis() + ".jpg");
+        generator.generateText(imagefile, to, image.getWidth() / 2, image.getHeight() / 2);
+        nimage = ImageIO.read(to); 
+        assertEquals("mismatched width", image.getWidth() / 2, nimage.getWidth());
+        assertEquals("mismatched height", image.getHeight() / 2, nimage.getHeight());
+    }
+
+    /**
+     * Tests resizing of generateText image
+     * @throws IOException output image couldn't be written / read / no outputfile
+     */
+    @Test
+    public void generateImageTestResize() throws IOException {
+        var to = new File("target/test/image_resized_" + System.currentTimeMillis() + ".jpg");
+        generator.generateImage(imagefile, to, false, image.getWidth() / 2, image.getHeight() / 2, "");
+        nimage = ImageIO.read(to);
+        assertEquals("mismatched width", image.getWidth() / 2, nimage.getWidth());
+        assertEquals("mismatched height", image.getHeight() / 2, nimage.getHeight());
+    }
+
+    /**
+     * Tests creating zips
+     * Test will Fail on all Systems with / as default Fileseperator (*nix) see Generator.java:113!!!
+     * @throws IOException output zip couldn't be written / read / no outputfile
+     */
+    @Test
+    public void createZipWithAbsolutePathTest() throws IOException {
+        var zipfile = new File("target/test/test_zip" + System.currentTimeMillis() + ".zip");
+        var files = new Vector<File>();
+        var imfile = new File("target/test/test_image" + System.currentTimeMillis() + ".jpg");
+        Files.copy(imagefile.toPath(), imfile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        files.add(imfile);
+        generator.createZip(zipfile, files);
+        var izip = new ZipInputStream(new FileInputStream(zipfile));
+        var entry = izip.getNextEntry();
+        assertEquals(imfile.getName(), entry.getName());
+        nimage = ImageIO.read(izip);
+        assertEquals(image.getWidth(), nimage.getWidth());
+        assertEquals(image.getHeight(), nimage.getHeight());
+        for (int j = 0; j < image.getHeight(); j++) {
+            for (int i = 0; i < image.getWidth(); i++) {
+                assertEquals("False Image i=" + i + " j=" + j, image.getRGB(i, j),
+                nimage.getRGB(i, j));
+            }
+        }
+        izip.closeEntry();
+        izip.close();
+    }
+
+    /**
+     * Tests rotate 90°
+     * @throws IOException failed to copy / modify temp file
+     * @throws NullPointerException Implementation Error handling not working without Main class
+     */
+    @Test
+    @Ignore("1. Not a double as Parameter"
+            + "2. Killswitch at Generator.java:690 have to throw an Error 90 radians isn't a valid rotation")
+    public void rotateImageTestRotateFile90() throws IOException, NullPointerException {
+        var imfile = new File("target/test/test_image" + System.currentTimeMillis() + ".zip");
+        Files.copy(imagefile.toPath(), imfile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        // redundant int cast, because deprecated Function signature
+        generator.rotate(imfile, (int)(Math.PI / 2));
+        nimage = ImageIO.read(imfile);
+        assertNotNull(nimage);
+        assertNotSame(nimage, image);
+        assertEquals(image.getHeight(), nimage.getWidth());
+        assertEquals(image.getWidth(), nimage.getHeight());
+        for (int j = 0; j < image.getHeight(); j++) {
+            for (int i = 0; i < image.getWidth(); i++) {
+                assertEquals("False Image i=" + i + " j=" + j, image.getRGB(i, j),
+                nimage.getRGB(nimage.getWidth() - 1 - j, i));
             }
         }
     }

91ed44633826648f09434ac659963788237c495e
Christopher Homberger <christopher.homberger@web.de>
2019-06-21 11:59:57 +0200

    Implemented the CoffeMachine

--- iMage/iMage.iLonghDe/src/main/java/org/iMage/iLonghDe/CoffeeMachine.java
+++ iMage/iMage.iLonghDe/src/main/java/org/iMage/iLonghDe/CoffeeMachine.java
@@ -8,28 +8,254 @@ import org.iMage.iLonghDe.base.IStateMachine;
  * name 3 faults
  */
 public class CoffeeMachine implements IStateMachine {
+    private enum State implements IState {
+        Standby {
+            @Override
+            public void entry(IStateMachine machine) {
+                machine.display("");
+            }
+
+            @Override
+            public void exit(IStateMachine machine) {
+
+            }
+
+            @Override
+            public State powerButtonPressed() {
+                return null;
+            }
+
+            @Override
+            public State standbyButtonPressed() {
+                return Waiting;
+            }
+
+            @Override
+            public State coffeeButtonPressed() {
+                return this;
+            }
+
+            @Override
+            public State cleaningButtonPressed() {
+                return this;
+            }
+        },
+        
+        Waiting {
+            @Override
+            public void entry(IStateMachine machine) {
+                machine.display("Waiting");
+            }
+
+            @Override
+            public void exit(IStateMachine machine) {
+
+            }
+
+            /**
+             * Realy why not turn off
+             */
+            @Override
+            public State powerButtonPressed() {
+                return this;
+            }
+
+            @Override
+            public State standbyButtonPressed() {
+                return Standby;
+            }
+
+            @Override
+            public State coffeeButtonPressed() {
+                return Brewing;
+            }
+
+            @Override
+            public State cleaningButtonPressed() {
+                return Cleaing;
+            }
+
+        },
+
+        Cleaing {
+            @Override
+            public void entry(IStateMachine machine) {
+                machine.display("Cleaning");
+            }
+
+            @Override
+            public void exit(IStateMachine machine) {
+
+            }
+
+            /**
+             * Realy why not turn off
+             */
+            @Override
+            public State powerButtonPressed() {
+                return this;
+            }
+
+            @Override
+            public State standbyButtonPressed() {
+                return Standby;
+            }
+
+            @Override
+            public State coffeeButtonPressed() {
+                return this;
+            }
+
+            @Override
+            public State cleaningButtonPressed() {
+                return Waiting;
+            }
+
+        },
+
+        Brewing {
+            @Override
+            public void entry(IStateMachine machine) {
+                machine.display("BrewCoffee");
+            }
+
+            @Override
+            public void exit(IStateMachine machine) {
+                machine.display("Done");
+            }
+
+            /**
+             * Realy why not turn off
+             */
+            @Override
+            public State powerButtonPressed() {
+                return this;
+            }
+
+            @Override
+            public State standbyButtonPressed() {
+                return Standby;
+            }
+
+            @Override
+            public State coffeeButtonPressed() {
+                return Waiting;
+            }
+
+            @Override
+            public State cleaningButtonPressed() {
+                return this;
+            }
+
+        },
+        /**
+         * State which does nothing than keep the state
+         * Could accept powerbutton to turn on in real world
+         */
+        Off {
+
+            @Override
+            public void entry(IStateMachine machine) {
+
+            }
+
+            @Override
+            public void exit(IStateMachine machine) {
+
+            }
+
+            @Override
+            public State powerButtonPressed() {
+                return this;
+            }
+
+            @Override
+            public State standbyButtonPressed() {
+                return this;
+            }
+
+            @Override
+            public State coffeeButtonPressed() {
+                return this;
+            }
+
+            @Override
+            public State cleaningButtonPressed() {
+                return this;
+            }
+
+        }
+        ;
+
+        /**
+         * entry function
+         */
+        public abstract void entry(IStateMachine machine);
+
+        /**
+         * exit function
+         */
+        public abstract void exit(IStateMachine machine);
+
+        /**
+         * power Button pressed
+         */
+        public abstract State powerButtonPressed();
+
+        /**
+         * standby Button pressed
+         */
+        public abstract State  standbyButtonPressed();
+
+        /**
+         * coffee Button pressed
+         */
+        public abstract State  coffeeButtonPressed();
+
+        /**
+         * cleaning Button pressed
+         */
+        public abstract State  cleaningButtonPressed();
+    }
+
+    private State currentstate;
+
+    public CoffeeMachine() {
+        setState(State.Standby);
+    }
+
+    private void setState(State state) {
+        if(currentstate != state) {
+            if(currentstate != null) {
+                currentstate.exit(this);
+            }
+            currentstate = state;
+            currentstate.entry(this);
+        }
+    }
+
     @Override
     public void cleaningButtonPressed() {
-
+        setState(currentstate.cleaningButtonPressed());
     }
 
     @Override
     public void coffeeButtonPressed() {
-
+        setState(currentstate.coffeeButtonPressed());
     }
 
     @Override
     public IState getCurrentState() {
-        return null;
+        return currentstate;
     }
 
     @Override
     public void powerButtonPressed() {
-
+        setState(currentstate.powerButtonPressed());
     }
 
     @Override
     public void standbyButtonPressed() {
-
+        setState(currentstate.standbyButtonPressed());
     }
 }
